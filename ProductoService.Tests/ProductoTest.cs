8f9e7bffcb8991de6b1dc8b0516504d489a8f734
airamtaleb <airamtaleb@gmail.com>
2022-10-13 12:45:12 +0200

    add ProductoServiceTests

---
+++ ProductoService.Tests/ProductoTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductoService.Controllers;
+using ProductoService.DAL;
+
+namespace ProductoService.Tests
+{
+
+    [TestClass]
+    public class ProductoTest
+    {
+        [TestMethod]
+        public void GetAsyncReturnsOk()
+        {
+            var productProvider = new ProductProvider();
+            var productController = new ProductController(productProvider);
+
+            var result = productController.GetAsync("1").Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
+
+        [TestMethod]
+        public void GetAsyncReturnsNotFound()
+        {
+            var productProvider = new ProductProvider();
+            var productController = new ProductController(productProvider);
+
+            var result = productController.GetAsync("1000").Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+    }
+}
