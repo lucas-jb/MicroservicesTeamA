627a40d78b7cc68917b66eca823aecc40af20bd5
enrique <123mailona@gmail.com>
2022-10-13 12:15:19 +0200

    ProveedorSv.01

---
+++ ProveedoresService/Models/Proveedor.cs
@@ -0,0 +1,12 @@
+﻿namespace ProveedoresService.Models
+{
+    public class Proveedor
+    {
+        public int ID { get; set; }
+        public String nombre { get; set; }
+        public String tipo { get; set; }
+        public String Ciudad { get; set; }
+
+
+    }
+}
