89291412badc18f583199059ff293a65a2ad466c
Lucas Juárez Barrios <lucasjuarezbarrios@gmail.com>
2022-10-21 12:43:15 +0200

    arreglar errores, todo OKAAYYYY!!!!!!

--- PruebaSearch/Models/Proveedor.cs
+++ PruebaSearch/Models/Proveedor.cs
@@ -1,10 +1,14 @@
-﻿namespace PruebaSearch.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PruebaSearch.Models
 {
     public class Proveedor
     {
-        public int ID { get; set; }
-        public String nombre { get; set; }
-        public String tipo { get; set; }
-        public String Ciudad { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string City { get; set; }
     }
 }
